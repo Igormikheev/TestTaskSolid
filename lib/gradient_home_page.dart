d8139f11e3a9f5793ccb951f41a8fbcd03c7a99d
IgorMikheev <igor7mih@gmail.com>
2020-07-12 14:57:23 +0300

    Add background random color

--- lib/gradient_home_page.dart
+++ lib/gradient_home_page.dart
@@ -7,8 +7,7 @@ class GradientHomePage extends StatefulWidget {
 }
 
 class GradientHomeState extends State<GradientHomePage> {
-  Color _colorGradientStart = UniqueColorGenerator.generateColor();
-  Color _colorGradientEnd = UniqueColorGenerator.generateColor();
+  Color _backgroundColor = UniqueColorGenerator.generateColor();
   Color _textColor = Colors.black;
   double _textFontSize = 22;
 
@@ -16,26 +15,20 @@ class GradientHomeState extends State<GradientHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: GestureDetector(
-          onTap: () {
-            setState(() {
-              _colorGradientStart = UniqueColorGenerator.generateColor();
-              _colorGradientEnd = UniqueColorGenerator.generateColor();
-            });
-          },
-          child: Container(
-            child: Align(
-                alignment: Alignment.center,
-                child: Text(
-                  "Hey there",
-                  style: TextStyle(color: _textColor, fontSize: _textFontSize),
-                )),
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: <Color>[_colorGradientStart, _colorGradientEnd],
-                )),
-          ),
-        ));
+      onTap: () {
+        setState(() {
+          _backgroundColor = UniqueColorGenerator.generateColor();
+        });
+      },
+      child: Container(
+        color: _backgroundColor,
+        child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              "Hey there",
+              style: TextStyle(color: _textColor, fontSize: _textFontSize),
+            )),
+      ),
+    ));
   }
 }
